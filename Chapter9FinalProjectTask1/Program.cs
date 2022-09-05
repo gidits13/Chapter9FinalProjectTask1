25c785c6312bae3dff8a50e22f57c86602f9177a
gidits13 <gidits13@gmail.com>
2022-09-05 10:35:21 +0300

    Добавьте файлы проекта.

---
+++ Chapter9FinalProjectTask1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace Chapter9FinalProjectTask1
+{
+    public class CustomException : Exception
+    {
+       public CustomException()
+        { }
+       public CustomException(string message) : base(message)
+        {
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] excarray = new Exception[5] { new ArgumentException("Неверный аргумент"), new CustomException("Custom Exception"),
+                new DivideByZeroException("Нельзя делить на 0") ,new DirectoryNotFoundException("Диретория не найдена"),new FormatException("Неверный формат") };
+            foreach(var ex in excarray)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        class CustomException : Exception
+        {
+            public CustomException()
+            { }
+            public CustomException(string message) : base(message)
+            {
+
+            }
+        }
+        public class Reader
+        {
+            public delegate void NumReader(int number, string[] lastNames);
+            public event NumReader OnNumReader;
+
+            public void NumberReader(string[] lastNames)
+            {
+                Console.WriteLine("Для сортировки А-Я нажмите 1. Для сортировки Я-А нажмите 2");
+
+                int number = Convert.ToInt32(Console.ReadLine());
+                if ((number != 1) && (number != 2))
+                { throw new CustomException("Введены неверные данные! Принимается только 1 или 2"); }
+
+                numberEntered(number, lastNames);
+            }
+            public void numberEntered(int number, string[] lastNames)
+            {
+                OnNumReader?.Invoke(number, lastNames);
+            }
+        }
+
+        static void SortArray(int number, string[] lastNames)
+        {
+            switch (number)
+            {
+                case 1:
+                    Array.Sort(lastNames);
+                    foreach(string name in lastNames)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    break;
+                    
+                case 2:
+                    Array.Sort(lastNames);
+                    Array.Reverse(lastNames);
+                    foreach (string name in lastNames)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    break;   
+            }      
+        }
+        static void Main(string[] args)
+        {
+            string[] lastNames = new string[5] { "Борисов", "Васильев", "Абрамов", "Григорьев", "Дмитриев" };
+            Reader reader = new Reader();
+            reader.OnNumReader += SortArray;
+            try
+            {
+                reader.NumberReader(lastNames);
+            }
+            catch(FormatException e)
+            { Console.WriteLine(e.Message); }
+            catch(CustomException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
